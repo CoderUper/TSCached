8782fedf27ec7c174b4aac741560dce3fc6f25de
CoderUper <jinzhuguang08@163.com>
2021-05-09 14:54:58 +0800

    增加grpc网络通信功能

--- main.cpp
+++ main.cpp
@@ -1,7 +1,47 @@
 #include "storage/tsmap.h"
-
+#include "protobuf/point.pb.h"
+#include "protobuf/point.grpc.pb.h"
+#include <grpcpp/grpcpp.h>
 #include <map>
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ClientContext;
+using grpc::Status;
+using TSCached::QueryRequest;
+using TSCached::QueryResponse;
+using TSCached::WriteRequest;
+using TSCached::WriteResponse;
+using TSCached::TSCachedService;
+
 using namespace std;
+
+class Sample  final : public TSCachedService::StubInterface{
+public:
+    Status QueryPoints(::grpc::ClientContext *context, const QueryRequest &request,
+                       ::TSCached::QueryResponse *response) override {
+        response->set_message("succusful");
+        return Status::OK;
+    }
+
+    Status InsertPoints(::grpc::ClientContext *context, const WriteRequest &request,
+                        ::TSCached::WriteResponse *response) override {
+        response->set_message("thanks");
+        return Status::OK;
+    }
+};
+
+void RunServer(){
+    string serveraddr("0.0.0.0:5501");
+    Sample s;
+
+    ServerBuilder serverBuilder;
+    serverBuilder.AddListeningPort(serveraddr,grpc::InsecureServerCredentials());
+    serverBuilder.RegisterService(reinterpret_cast<grpc::Service *>(&servive));
+
+}
+
 int main() {
     TSCached::TSMap *tsMap = new TSCached::TSMap();
     string db = "test";
